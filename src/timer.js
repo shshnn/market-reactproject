779cd551788ee9cc6a7a93ddf3442c1bedf6f3a6
shshnn <ssss5489@naver.com>
2023-10-29 21:39:51 +0900

    project init

---
+++ src/timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TimerComponent(){
+    const [time, setTime] = React.useState(0);
+    console.log("컴포넌트 업데이트");
+   React.useEffect(function (){
+    setTime(time + 1);
+   },[]);
+    return <div>
+        <h3>{time}초</h3>
+        <button >1씩올려주세요</button>
+    </div>
+}
+export default TimerComponent; 
