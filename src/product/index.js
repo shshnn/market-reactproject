ad4a7f080a0ec94072f43ad23e4b601141184325
shshnn <ssss5489@naver.com>
2023-11-03 18:59:35 +0900

    결제하기 구현

--- src/product/index.js
+++ src/product/index.js
@@ -2,34 +2,56 @@ import { useParams } from "react-router-dom/cjs/react-router-dom";
 import axios from "axios";
 import { useEffect,useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants.js";
+import dayjs from "dayjs";
+import { Button, message } from "antd";
+
 function ProductPage() {
     const {id}= useParams();
     const [product, setProduct] = useState(null);
     
-    
+    const getProduct = () => {
+        axios.get(`${API_URL}/products/${id}`)
+        .then(function(result){
+            
+            setProduct(result.data.product);
+            console.log(result)
+            
+        }).catch(function(error){
+       
+            console.log('2222');
+                 console.error(error);
+        
+        });
+    }
+
     useEffect(function(){
 
  
-    axios.get(`https://1357fe84-d97b-454d-9475-0ca1806a88e6.mock.pstmn.io/products/${id}`).then(function(result){
-        
-        setProduct(result.data);
-        console.log(result)
-        
-    }).catch(function(error){
-   
-        console.log('2222');
-             console.error(error);
-    
-    });
+        getProduct();
+
 },[]);
 
 if(product == null){
     return <h1>상품 정보를 받고 있습니다...</h1>
 }
+
+
+const onClickPurchase = () => {
+    axios.post(`${API_URL}/purchase/${id}`)
+    .then((result) =>{
+        getProduct();
+        message.info("구매가 완료되었습니다");
+    }).catch((error) => {
+        message.error(`에러가 발생했습니다 ${error.message}`);
+        
+    });
+}
+
     return (
     <div>
         <div id="image-box">
-        <img src={"/"+product.imageUrl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
         </div>
         <div id="profile-box">
             <img src="/images/images/icons/avatar.png"/>
@@ -38,8 +60,15 @@ if(product == null){
         <div id="contents-box">
             <div id="name">{product.name}</div>
             <div id="price">{product.price}원</div>
-            <div id="createdAt">2023년 10월 30일</div>
-            <div id="description">{product.description}</div>
+            <div id="createdAt">{dayjs(product.createdAt).format("YYYY년 MM월 DD일")}</div>
+            <Button 
+            id="purchase-button"
+            size="large"
+            type="primary"
+            danger 
+            onClick={onClickPurchase}
+            disabled={product.soldout === 1} >구매하기</Button>
+            <pre id="description">{product.description}</pre>
         </div>
     </div>
     );
